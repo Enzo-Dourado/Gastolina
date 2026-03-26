60a13a1d6f36d4e785f375e8db2a85d2f641426d
Enzo Dourado <enzodourado.cripto@gmail.com>
2026-03-26 01:41:08 -0300

    feat: Initial FIPE API consumption system

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import br.com.projeto.gastolina.models.ConsumoDaAPI;
+
+
+import java.util.Scanner;
+
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ConsumoDaAPI consumo = new ConsumoDaAPI();
+
+        System.out.println("Digite que tipo de veiculo quer analisar (cars, motorcycles, trucks): ");
+        consumo.setTipoVeiculo(sc.next());
+        consumo.consumoDaAPI();
+
+    }
+
 }
---
+++ src/br/com/projeto/gastolina/models/ConsumoDaAPI.java
@@ -0,0 +1,45 @@
+package br.com.projeto.gastolina.models;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoDaAPI extends Veiculos{
+    private String url;
+    private String json;
+
+    public String getUrl() {
+        this.url = "https://fipe.parallelum.com.br/api/v2/" + getTipoVeiculo() + "/brands";
+        return url;
+    }
+
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+
+    public void consumoDaAPI(){
+        // Consome a API FIPE
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(getUrl()))
+                    .build();
+
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            this.json = response.body();
+            System.out.println(json);
+
+        }  catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
